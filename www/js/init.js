252c84543105f11aca1a38a5d89a0f478233d1cf
Marc <marcgalaxy02@gmail.com>
2022-02-25 17:10:35 +0100

    parallax commit2

---
+++ www/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.tabs').tabs({"swipeable":true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('deviceready', onDeviceReady, false);
